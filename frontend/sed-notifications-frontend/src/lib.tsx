34cd34a04536d6c69804968f5b375dff21f7ce2b
azerq <azerqdev@gmail.com>
2025-11-06 09:03:05 +0300

    refactor(notifications): remove wrong library entry point file

--- frontend/sed-notifications-frontend/src/lib.tsx
+++
@@ -1,7 +0,0 @@
-// Main library entry point
-export { NotificationsStore } from './store/NotificationsStore';
-export { useSignalRConnection } from './hooks/useSignalRConnection';
-export { default as NotificationBell } from './NotificationsBar/NotificationBell';
-export { default as NotificationList } from './NotificationsBar/NotificationList';
-export { default as NotificationItem } from './NotificationsBar/NotificationItem';
-export type { Notification } from './models/Notification';
